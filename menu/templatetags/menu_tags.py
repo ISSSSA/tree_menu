604aeb2d902ccc669c5eaa1cd2e74bc91dd95383
ISSSSA <52818700+ISSSSA@users.noreply.github.com>
2024-10-11 00:48:57 +0300

    fix deep

--- menu/templatetags/menu_tags.py
+++ menu/templatetags/menu_tags.py
@@ -1,13 +1,12 @@
 from django import template
 from ..models import MenuItem
-from django.urls import resolve
 
 register = template.Library()
 
 
 @register.inclusion_tag('menu.html', takes_context=True)
 def draw_menu(context, menu_name):
-    current_url = resolve(context['request'].path_info).url_name
+    current_url = context['request'].path_info
     menu_items = MenuItem.objects.filter(menu_name=menu_name).select_related('parent')
 
     def build_menu_tree(items, parent=None):
@@ -15,12 +14,14 @@ def draw_menu(context, menu_name):
         for item in items:
             if item.parent == parent:
                 children = build_menu_tree(items, parent=item)
+                is_active = current_url.startswith(item.get_url())  # Активен ли этот пункт меню
                 tree.append({
                     'item': item,
                     'children': children,
-                    'is_active': current_url == item.named_url
+                    'is_active': is_active or any(child['is_active'] for child in children)
+                    # Активен или есть активные потомки
                 })
         return tree
 
     menu_tree = build_menu_tree(menu_items)
-    return {'menu_tree': menu_tree}
+    return {'menu_tree': menu_tree, 'current_url': current_url}
